44b83c6e2a4ff294db1cfa7344c2ce5cffbb37fb
kouczi <kacperskaiii@gmail.com>
2025-04-09 14:41:46 +0200

    No error

--- src/main.py
+++ src/main.py
@@ -1,20 +1,3 @@
 def hello_world():
-print("Hello, world!") # Fixed indentation
+    print("Hello, world!") # Fixed indentation
 hello_world()
-
-test = [1,2,3,4,5
-        ,7,8,9,10,
-        11,12,13]
-
-def tescik (   e   ):
-    print(e)
-
-very_long_variable_name = True
-if very_long_variable_name is not None  and \
- very_long_variable_name.field > 0 or \
- type(very_long_variable_name) == bool:
- z = "hello"+"world"
-
-print(aadoadkoadoaokaoa)
-print("aokdaodaokadsoasdoadkdkoasdkosko")
-print("adfdfasdsds")
